73141cb328b0933c2d30d078700e1a6736a7bb09
Isaiah Fuller <9921699+isaiahfuller@users.noreply.github.com>
2026-07-16 14:55:07 -0400

    feat: add functionality to unlink GitHub account in settings and update UI button state

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,34 +1,35 @@
+import { redirect } from 'next/navigation';
+import { Button, Center, Container, Divider, Group, Stack, Text, Title } from '@mantine/core';
 import AccountDeleteButton from '@/components/AuthButtons/AccountDelete';
 import AniListButton from '@/components/AuthButtons/Anilist';
 import GithubButton from '@/components/AuthButtons/Github';
 import MalButton from '@/components/AuthButtons/Mal';
 import { createClient } from '@/lib/supabase/server';
-import { Button, Center, Container, Divider, Group, Stack, Title } from "@mantine/core";
-import { Text } from "@mantine/core";
-import { redirect } from 'next/navigation';
 
 export default async function Settings() {
   const supabase = await createClient();
-  const { data: { user } } = await supabase.auth.getUser();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
   if (!user) {
     return redirect('/login');
   }
   const { data: identities, error } = await supabase.auth.getUserIdentities();
-  const linkedProviders = identities?.identities?.map((identity) => identity.provider)
+  const linkedProviders = identities?.identities?.map((identity) => identity.provider);
   // console.log(identities?.identities)
   if (identities?.identities) {
     for (const identity of identities?.identities) {
-      if (identity.provider === "custom:anilist") {
-        console.log("AniList linked");
-        console.log(identity.identity_data)
+      if (identity.provider === 'custom:anilist') {
+        console.log('AniList linked');
+        console.log(identity.identity_data);
       }
-      if (identity.provider === "custom:myanimelist") {
-        console.log("MyAnimeList linked");
-        console.log(identity.identity_data)
+      if (identity.provider === 'custom:myanimelist') {
+        console.log('MyAnimeList linked');
+        console.log(identity.identity_data);
       }
-      if (identity.provider === "github") {
-        console.log("GitHub linked");
-        console.log(identity.identity_data)
+      if (identity.provider === 'github') {
+        console.log('GitHub linked');
+        console.log(identity.identity_data);
       }
     }
   }
@@ -43,15 +44,30 @@ export default async function Settings() {
         <Text>Link your social accounts to get started</Text>
         <Group>
           <Text>AniList</Text>
-          {linkedProviders?.includes("custom:anilist") ? <Text>Already linked</Text> : <AniListButton loggedIn={true} />}
+          {linkedProviders?.includes('custom:anilist') ? (
+            <Text>Already linked</Text>
+          ) : (
+            <AniListButton loggedIn={true} />
+          )}
         </Group>
         <Group>
           <Text>MyAnimeList</Text>
-          {linkedProviders?.includes("custom:myanimelist") ? <Text>Already linked</Text> : <MalButton loggedIn={true} />}
+          {linkedProviders?.includes('custom:myanimelist') ? (
+            <Text>Already linked</Text>
+          ) : (
+            <MalButton loggedIn={true} />
+          )}
         </Group>
         <Group>
           <Text>GitHub</Text>
-          {linkedProviders?.includes("github") ? <Text>Already linked</Text> : <GithubButton loggedIn={true} />}
+          {linkedProviders?.includes('github') ? (
+            <GithubButton
+              loggedIn={true}
+              provider={identities?.identities.find((identity) => identity.provider === 'github')}
+            />
+          ) : (
+            <GithubButton loggedIn={true} />
+          )}
         </Group>
       </Stack>
       <Divider />
--- components/AuthButtons/Github.tsx
+++ components/AuthButtons/Github.tsx
@@ -1,17 +1,27 @@
 'use client';
 
+import { redirect } from 'next/navigation';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { UserIdentity } from '@supabase/supabase-js';
 import { Button } from '@mantine/core';
 import { createClient } from '@/lib/supabase/client';
 
-export default function GithubButton({ loggedIn = false }: { loggedIn: boolean }) {
+export default function GithubButton({
+  loggedIn = false,
+  provider = null,
+}: {
+  loggedIn: boolean;
+  provider?: null | UserIdentity;
+}) {
   const supabase = createClient();
   async function signInWithGithub() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'github',
       options: {
-        redirectTo: loggedIn ? `${window.location.origin}/media-rss/settings` : `${window.location.origin}/media-rss/auth/callback`,
+        redirectTo: loggedIn
+          ? `${window.location.origin}/media-rss/settings`
+          : `${window.location.origin}/media-rss/auth/callback`,
       },
     });
 
@@ -21,6 +31,15 @@ export default function GithubButton({ loggedIn = false }: { loggedIn: boolean }
     }
   }
   async function linkGithub() {
+    if (provider) {
+      const { data, error } = await supabase.auth.unlinkIdentity(provider);
+      if (error) {
+        // eslint-disable-next-line no-console
+        console.error(error);
+      }
+      await supabase.auth.signOut();
+      redirect('/login');
+    }
     const { error } = await supabase.auth.linkIdentity({
       provider: 'github',
       options: {
@@ -35,12 +54,12 @@ export default function GithubButton({ loggedIn = false }: { loggedIn: boolean }
   }
   return (
     <Button
-      justify='space-between'
+      justify="space-between"
       leftSection={<FontAwesomeIcon icon={faGithub} />}
       onClick={loggedIn ? linkGithub : signInWithGithub}
       rightSection={<span />}
     >
-      {loggedIn ? 'Link GitHub' : 'Sign in with GitHub'}
+      {loggedIn ? (provider ? 'Unlink GitHub' : 'Link GitHub') : 'Sign in with GitHub'}
     </Button>
   );
 }
